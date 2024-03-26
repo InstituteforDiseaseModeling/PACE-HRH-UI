2c44a246c49e666161d250b17964639fa583504f
mewu <mewu@idmod.org>
2024-03-26 01:55:23 -0700

    fix remark issue

--- install_pace_ui.bat
+++ install_pace_ui.bat
@@ -3,7 +3,7 @@ setlocal
 @echo off
 
 
-:: Determine if this a git repository
+REM Determine if this a git repository
 IF EXIST ".git" (
     ECHO You are in a Git repository, please follow ReadMe Instruction to run app in Rstudio
     ECHO Press any key to terminate...
@@ -12,7 +12,7 @@ IF EXIST ".git" (
 ) 
 
 
-:: Determine if this is offline mode
+REM Determine if this is offline mode
 SET "OFFLINE_FOLDERS=R-4.2.2 R config"
 SET offline=TRUE
 FOR %%F IN (%OFFLINE_FOLDERS%) DO (
@@ -50,7 +50,7 @@ IF "%root_dir%"=="" (
 )
 
 
-:: Download PACE-HRH-UI Release 
+REM Download PACE-HRH-UI Release 
 SET APP_DIR=%root_dir%\PACE-HRH-UI
 SET CODE_URL=https://github.com/InstituteforDiseaseModeling/PACE-HRH-UI/archive/refs/tags/1.0.0.zip
 SET DOWNLOAD_PATH="%root_dir%\pace-hrh-ui.zip"
@@ -72,7 +72,7 @@ tar -xf %DOWNLOAD_PATH% -C "%APP_DIR%"
 
 
 PUSHD "%APP_DIR%"
-:: Change to the first directory found (extracted from zip)
+REM Change to the first directory found (extracted from zip)
 for /d %%G in (*) do (
     SET WORKING_DIR=%%G
     ECHO Working directory in "%%G"
@@ -94,7 +94,7 @@ ECHO.
 :askChoice
 SET /p userChoice=Your choice (accept/cancel): 
 
-:: Convert the input to lowercase for comparison
+REM Convert the input to lowercase for comparison
 IF /i "%userChoice%"=="accept" GOTO accepted
 IF /i "%userChoice%"=="cancel" GOTO cancelled
 
@@ -109,32 +109,35 @@ POPD
 
 SET R_PATH="%APP_DIR%\%WORKING_DIR%\R-4.2.2\bin\Rscript.exe"
 SET SHINY_DIR="%APP_DIR%\%WORKING_DIR%"
+ECHO %R_PATH%
+ECHO "%SHINY_DIR%"
 
-:: Check if R is installed
+REM Check if R is installed
 SET DOWNLOAD_R_PATH="%root_dir%\R-4.2.2-win.exe"
+ECHO %DOWNLOAD_R_PATH%
 IF EXIST "%R_PATH%" (
     ECHO R is already installed.
 ) ELSE (
     IF NOT EXIST %DOWNLOAD_R_PATH% (
        ECHO Downloading R 4.2.2...
-       :: Replace the URL with the direct download link to the R installer you want to use
+       REM Replace the URL with the direct download link to the R installer you want to use
        curl -o %DOWNLOAD_R_PATH% https://cran.r-project.org/bin/windows/base/old/4.2.2/R-4.2.2-win.exe
     )
 
     ECHO Installing R...
-    :: Run the installer silently
+    REM Run the installer silently
     %DOWNLOAD_R_PATH% /VERYSILENT /NORESTART /DIR="%APP_DIR%\%WORKING_DIR%\R-4.2.2"
 
     ECHO R 4.2.2 installation complete.
 
     ECHO Install packages...
-    :: Run the R script in silent mode
+    REM Run the R script in silent mode
    
-    %R_PATH% --vanilla "%APP_DIR%\%WORKING_DIR%\install_packages.R"
+    %R_PATH% --verbose --vanilla "%APP_DIR%\%WORKING_DIR%\install_packages.R"
 )
 
 :app
-:: Start The shinyapps in port 8888 and open the browser
+REM Start The shinyapps in port 8888 and open the browser
 SET PORT=8888
 
 ECHO Starting Shiny app on port %PORT%...
@@ -142,11 +145,11 @@ ECHO Using %R_PATH% ON %SHINY_DIR%
 
 START /MIN "" %R_PATH% -e "shiny::runApp(appDir='%SHINY_DIR%', port=%PORT%, launch.browser=FALSE)"
 
-:: Wait for a few seconds to ensure the Shiny app has started
+REM Wait for a few seconds to ensure the Shiny app has started
 ECHO Wait for 5 seconds for app launching ...
 TIMEOUT /T 5
 
-:: Open the default web browser to the Shiny app
+REM Open the default web browser to the Shiny app
 START http://localhost:%PORT%
 
 GOTO end
@@ -158,11 +161,11 @@ PAUSE > nul
 exit
 
 :end
-:: Prompt the user to close the Shiny app
+REM Prompt the user to close the Shiny app
 ECHO Press any key to terminate the PACE-HRH app...
 PAUSE > nul
 
-:: Find and terminate the Rscript process that runs the Shiny app
+REM Find and terminate the Rscript process that runs the Shiny app
 ECHO Terminating the Shiny app...
 TASKKILL /IM Rscript.exe /f 2>nul
 
